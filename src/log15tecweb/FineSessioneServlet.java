acf06355c49a328a5525688dfee7e922118057e6
Administrator <Administrator@192.168.1.85>
2016-01-04 18:19:44 +0100

    Sto imparando a usare git xD (forse)

---
+++ src/log15tecweb/FineSessioneServlet.java
@@ -0,0 +1,48 @@
+package log15tecweb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FineSessioneServlet
+ */
+@WebServlet("/FineSessioneServlet")
+public class FineSessioneServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FineSessioneServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter out=response.getWriter();
+		out.print("<div class='messages error'>Sessione scaduta, rieffettuare il login</div>");
+		RequestDispatcher rd=request.getRequestDispatcher("index.html");
+	    rd.include(request,response);
+		//response.sendRedirect("index.html");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
